0c47e7accfa57dc94a55fab0295beee3cfb7f86a
Stephane Bruckert <contact@stephanebruckert.com>
2015-06-04 13:31:37 +0200

    generate-all gmines.Game

---
+++ grails-app/controllers/gmines/GameController.groovy
@@ -0,0 +1,107 @@
+package gmines
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class GameController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Game.list(params), model:[gameCount: Game.count()]
+    }
+
+    def show(Game game) {
+        respond game
+    }
+
+    def create() {
+        respond new Game(params)
+    }
+
+    @Transactional
+    def save(Game game) {
+        if (game == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (game.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond game.errors, view:'create'
+            return
+        }
+
+        game.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'game.label', default: 'Game'), game.id])
+                redirect game
+            }
+            '*' { respond game, [status: CREATED] }
+        }
+    }
+
+    def edit(Game game) {
+        respond game
+    }
+
+    @Transactional
+    def update(Game game) {
+        if (game == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        if (game.hasErrors()) {
+            transactionStatus.setRollbackOnly()
+            respond game.errors, view:'edit'
+            return
+        }
+
+        game.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'game.label', default: 'Game'), game.id])
+                redirect game
+            }
+            '*'{ respond game, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Game game) {
+
+        if (game == null) {
+            transactionStatus.setRollbackOnly()
+            notFound()
+            return
+        }
+
+        game.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'game.label', default: 'Game'), game.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'game.label', default: 'Game'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
