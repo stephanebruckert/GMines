e39ada1d151231ec2a3d3dc879c7080381c9942f
Stephane Bruckert <contact@stephanebruckert.com>
2015-06-08 17:42:30 +0200

    mapping change, game controller is now on index

--- grails-app/controllers/UrlMappings.groovy
+++ grails-app/controllers/UrlMappings.groovy
@@ -7,7 +7,7 @@ class UrlMappings {
             }
         }
 
-        "/"(view:"/index")
+        "/" (controller:"game", action:"index")
         "500"(view:'/error')
         "404"(view:'/notFound')
     }
